2ca5de9f902510d5a29aaf1ca8f945e203ae2942
Dan Ploesteanu <ploesteanudan.99@gmail.com>
2024-02-24 22:29:00 +0200

    form

--- scripts.js
+++ scripts.js
@@ -2,11 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
   // Select both buttons by using querySelectorAll
   const goToFormButtons = document.querySelectorAll('.form-button');
 
-  // Loop through all the buttons and attach event listeners
-  goToFormButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-      // Redirect to the form.html page
-      window.location.href = 'form.html';
+  if (goToFormButtons) {
+    // Loop through all the buttons and attach event listeners
+    goToFormButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        // Redirect to the form.html page
+        window.location.href = 'form.html';
+      });
     });
-  });
+  }
+
+  const goBackButton = document.querySelector('.go-back-button');
+
+  if (goBackButton) {
+    // Attach an event listener to the button
+    goBackButton.addEventListener('click', function () {
+      // Go back in the browser's history
+      window.history.back();
+    });
+  }
+
 });
